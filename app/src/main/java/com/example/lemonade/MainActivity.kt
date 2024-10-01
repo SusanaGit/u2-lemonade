0fadc1ba1d9ef536540f08a6764412f66fe64afb
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-10-01 11:35:55 +0200

    update image design

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -5,14 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,7 +45,12 @@ private fun Lemonade(
     ) {
         Image(
             painter = painterResource(R.drawable.lemon_tree),
-            contentDescription = "1"
+            contentDescription = "1",
+            modifier = Modifier
+                .background(Color(0xFFC9F8AD), RoundedCornerShape(20.dp))
+                .padding(20.dp)
+                .height(150.dp)
+                .width(150.dp)
         )
     }
 }
