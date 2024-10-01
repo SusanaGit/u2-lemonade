9dc309d3febb4b0ff2edf7b044dd155526e7c035
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-10-01 11:23:15 +0200

    configuring canvas

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -4,7 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lemonade.ui.theme.LemonadeTheme
 
@@ -21,8 +26,16 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-private fun Lemonade() {
+private fun Lemonade(
+    modifier: Modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center)
+) {
+    Column(
+        modifier = modifier
+    ) {
 
+    }
 }
 
 @Preview(showBackground = true)
