c46fcd1d1aeaf7880aa687fdc564459c933d6740
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-10-01 14:53:43 +0200

    create ShowView method

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -50,6 +50,9 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun Lemonade() {
+
+    var currentStep by remember { mutableStateOf(R.string.lemon_tree) }
+
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
@@ -71,7 +74,6 @@ private fun Lemonade() {
                 .wrapContentSize(Alignment.Center),
             color = MaterialTheme.colorScheme.background
         ) {
-            var currentStep by remember { mutableStateOf(R.string.lemon_tree) }
 
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally
@@ -79,96 +81,85 @@ private fun Lemonade() {
                 when (currentStep) {
 
                     R.string.lemon_tree -> {
-                        Image(
-                            painter = painterResource(R.drawable.lemon_tree),
-                            contentDescription = stringResource(R.string.lemon_tree),
-                            modifier = Modifier
-                                .clickable {
-                                    currentStep = R.string.lemon
-                                }
-                                .background(Color(0xFFBEEBBE), RoundedCornerShape(20.dp))
-                                .width(200.dp)
-                                .height(200.dp)
-                        )
-
-                        Spacer(
-                            modifier = Modifier.height(20.dp)
-                        )
-
-                        Text (
-                            text = stringResource(R.string.lemon_tree_text)
+                        ShowView(
+                            nameImageId = R.drawable.lemon_tree,
+                            descriptionImageId = R.string.lemon_tree,
+                            nextStepId = R.string.lemon,
+                            textImageId = R.string.lemon_tree_text,
+                            onStepChange = {
+                                newStep -> currentStep = newStep
+                            }
                         )
                     }
 
                     R.string.lemon -> {
-                        Image(
-                            painter = painterResource(R.drawable.lemon_squeeze),
-                            contentDescription = stringResource(R.string.lemon),
-                            modifier = Modifier
-                                .clickable {
-                                    currentStep = R.string.glass_of_lemonade
-                                }
-                                .background(Color(0xFFBEEBBE), RoundedCornerShape(20.dp))
-                                .width(200.dp)
-                                .height(200.dp)
-                        )
-
-                        Spacer(
-                            modifier = Modifier.height(20.dp)
-                        )
-
-                        Text (
-                            text = stringResource(R.string.lemon_text)
+                        ShowView(
+                            nameImageId = R.drawable.lemon_squeeze,
+                            descriptionImageId = R.string.lemon,
+                            nextStepId = R.string.glass_of_lemonade,
+                            textImageId = R.string.lemon_text,
+                            onStepChange = {
+                                    newStep -> currentStep = newStep
+                            }
                         )
                     }
 
                     R.string.glass_of_lemonade -> {
-                        Image(
-                            painter = painterResource(R.drawable.lemon_drink),
-                            contentDescription = stringResource(R.string.glass_of_lemonade),
-                            modifier = Modifier
-                                .clickable {
-                                    currentStep = R.string.empty_glass
-                                }
-                                .background(Color(0xFFBEEBBE), RoundedCornerShape(20.dp))
-                                .width(200.dp)
-                                .height(200.dp)
-                        )
-
-                        Spacer(
-                            modifier = Modifier.height(20.dp)
-                        )
-
-                        Text (
-                            text = stringResource(R.string.glass_of_lemonade_text)
+                        ShowView(
+                            nameImageId = R.drawable.lemon_drink,
+                            descriptionImageId = R.string.glass_of_lemonade,
+                            nextStepId = R.string.empty_glass,
+                            textImageId = R.string.glass_of_lemonade_text,
+                            onStepChange = {
+                                    newStep -> currentStep = newStep
+                            }
                         )
                     }
 
                     R.string.empty_glass -> {
-                        Image(
-                            painter = painterResource(R.drawable.lemon_restart),
-                            contentDescription = stringResource(R.string.empty_glass),
-                            modifier = Modifier
-                                .clickable {
-                                    currentStep = R.string.lemon_tree
-                                }
-                                .background(Color(0xFFBEEBBE), RoundedCornerShape(20.dp))
-                                .width(200.dp)
-                                .height(200.dp)
-                        )
-
-                        Spacer(
-                            modifier = Modifier.height(20.dp)
-                        )
-
-                        Text (
-                            text = stringResource(R.string.empty_glass_text)
+                        ShowView(
+                            nameImageId = R.drawable.lemon_restart,
+                            descriptionImageId = R.string.empty_glass,
+                            nextStepId = R.string.lemon_tree,
+                            textImageId = R.string.empty_glass_text,
+                            onStepChange = {
+                                    newStep -> currentStep = newStep
+                            }
                         )
                     }
                 }
             }
         }
     }
+}
+
+@Composable
+private fun ShowView(
+    nameImageId: Int,
+    descriptionImageId: Int,
+    nextStepId: Int,
+    textImageId: Int,
+    onStepChange: (Int) -> Unit
+) {
+    Image(
+        painter = painterResource(nameImageId),
+        contentDescription = stringResource(descriptionImageId),
+        modifier = Modifier
+            .clickable {
+                onStepChange(nextStepId)
+            }
+            .background(Color(0xFFBEEBBE), RoundedCornerShape(20.dp))
+            .width(200.dp)
+            .height(200.dp)
+    )
+
+    Spacer(
+        modifier = Modifier.height(20.dp)
+    )
+
+    Text (
+        text = stringResource(textImageId)
+    )
 }
 
 @Preview(showBackground = true)
